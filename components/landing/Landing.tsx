33335752b0da68f2cef047b5065b918706bf2c38
Yukesh Dhakal <yukeshdhakal42@gmail.com>
2024-12-23 21:56:58 +0545

    feat: add info section in landing page

--- components/landing/Landing.tsx
+++ components/landing/Landing.tsx
@@ -1,9 +1,11 @@
 import Hero from "./hero/Hero";
+import Info from "./info/Info";
 
 const Landing = () => {
   return (
     <>
       <Hero />
+      <Info />
     </>
   );
 };
--- components/landing/hero/Hero.tsx
+++ components/landing/hero/Hero.tsx
@@ -3,10 +3,10 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="web-container py-8 lg:py-12 flex flex-col lg:flex-row gap-4 lg:gap-8 items-center">
+    <section className="web-container py-8 lg:py-20 flex flex-col lg:flex-row gap-4 lg:gap-8 items-center">
       <div className="flex flex-col gap-4 lg:gap-8 flex-1">
         <div className="flex flex-col gap-3 lg:gap-4">
-          <h2 className="text-4xl md:text-5xl lg:text-7xl leading-tight font-[500]">
+          <h2 className="text-4xl md:text-5xl lg:text-7xl lg:leading-tight font-[500]">
             Choose your <br />
             Coffee & <br />
             Space
@@ -22,7 +22,7 @@ const Hero = () => {
           width={1200}
           height={600}
           alt="hero"
-          className="w-full hidden h-[250px] md:h-[350px] md:block lg:h-[490px] object-cover object-center"
+          className="w-full hidden h-[250px] md:h-[350px] md:block lg:h-[435px] object-cover object-center"
         />
       </div>
       <Image
